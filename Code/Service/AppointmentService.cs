0f0b820dc3bba345291559ec9068324738f921e2
LukaAndric <81173650+LukaAndric@users.noreply.github.com>
2022-04-07 16:57:32 +0200

    Jos koda

---
+++ Code/Service/AppointmentService.cs
@@ -0,0 +1,40 @@
+/***********************************************************************
+ * Module:  AppointmentService.cs
+ * Author:  lukaa
+ * Purpose: Definition of the Class Service.AppointmentService
+ ***********************************************************************/
+
+using System;
+
+namespace Service
+{
+   public class AppointmentService
+   {
+      public Boolean CreateApp(DateTime dateTime)
+      {
+         // TODO: implement
+         return null;
+      }
+      
+      public Boolean UpdateApp(DateTime dateTime)
+      {
+         // TODO: implement
+         return null;
+      }
+      
+      public Boolean DeleteApp(int id)
+      {
+         // TODO: implement
+         return null;
+      }
+      
+      public Appointment ReadApp(int id)
+      {
+         // TODO: implement
+         return null;
+      }
+   
+      public Repository.Appointment appointment;
+   
+   }
+}
