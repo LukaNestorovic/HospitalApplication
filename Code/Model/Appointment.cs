576be1f7917d78e3d4ef28f4ff809b8400e40458
LukaAndric <81173650+LukaAndric@users.noreply.github.com>
2022-04-07 19:28:46 +0200

    kod

--- Code/Model/Appointment.cs
+++ Code/Model/Appointment.cs
@@ -16,11 +16,15 @@ namespace Model
          return 0.0F;
       }
    
+      public Doctor doctor;
+      public Patient patient;
+   
       private DateTime DateTime;
       private String Descripton;
       private int Duration;
       private Boolean Emergency;
       private int Id;
+      private Boolean Taken;
    
    }
 }
