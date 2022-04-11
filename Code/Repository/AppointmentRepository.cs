814ef845828d856969302b1a6d6ab253b08396fc
Jovan <savkovic.jovan@protonmail.com>
2022-04-11 15:23:30 +0200

    repository za appointment

--- Code/Repository/AppointmentRepository.cs
+++ Code/Repository/AppointmentRepository.cs
@@ -1,6 +1,6 @@
 /***********************************************************************
- * Module:  AppointmentRepository.cs
- * Author:  lukaa
+ * Module:	AppointmentRepository.cs
+ * Author:	lukaa
  * Purpose: Definition of the Class Repository.AppointmentRepository
  ***********************************************************************/
 
@@ -10,40 +10,63 @@ using Model;
 
 namespace Repository
 {
-   public class AppointmentRepository
-   {
-      public List<Appointment> FindAll()
-      {
-         // TODO: implement
-         return null;
-      }
-      
-      public Appointment FindByID(int id)
-      {
-         // TODO: implement
-         return null;
-      }
-      
-      public Boolean Save(Appointment appointment)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean DeleteByID(int id)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean UpdateByID(Appointment appointment)
-      {
-         // TODO: implement
-         return false;
-      }
-   
-      private String FileName;
-
-   
-   }
+	public class AppointmentRepository
+	{
+		public List<Appointment> FindAll()
+		{
+			return serializer.fromJSON(FileName);
+		}
+		
+		public Appointment FindByID(int id)
+		{
+			List<Appointment> all = serializer.fromJSON(FileName);
+			Appointment a;
+			foreach(Appointment i in all){
+				if(i.Id == id){
+					a = i;
+					break;
+				}
+			}
+			return a;
+		}
+		
+		public Boolean Save(Appointment appointment)
+		{
+			List<Appointment> all = serializer.fromJSON(FileName);
+			all.Add(appointment);
+			serializer.toJSON(FileName, all);
+			return true;
+		}
+		
+		public Boolean DeleteByID(int id)
+		{
+			List<Appointment> all = serializer.fromJSON(FileName);
+			foreach(Appointment i in all){
+				if(i.Id == id){
+					all.Remove(i);
+					break;
+				}
+			}
+			serializer.toJSON(FileName, all);
+			return true;
+		}
+		
+		public Boolean UpdateByID(Appointment appointment)
+		{
+			List<Appointment> all = serializer.fromJSON(FileName);
+			foreach(Appointment i in all){
+				if(i.Id == appointment.Id){
+					i = appointment;
+					break;
+				}
+			}
+			serializer.toJSON(FileName, all);
+			return false;
+		}
+	
+		private static String FileName = "Appointments.json";
+		
+		private static Serializer<Appointment> serializer = new Serializer<Appointment>();
+	
+	}
 }
