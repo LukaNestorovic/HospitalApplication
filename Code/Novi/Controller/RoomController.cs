cf0fe4586222b6c9aeff4f799d56a8cc9fe30561
Jovan <savkovic.jovan@protonmail.com>
2022-04-12 14:53:07 +0200

    dodata generacija id i room controller

--- Code/Novi/Controller/RoomController.cs
+++ Code/Novi/Controller/RoomController.cs
@@ -1,6 +1,6 @@
 /***********************************************************************
- * Module:  RoomController.cs
- * Author:  SN CAR CODING
+ * Module:	RoomController.cs
+ * Author:	SN CAR CODING
  * Purpose: Definition of the Class Controller.RoomController
  ***********************************************************************/
 
@@ -10,33 +10,29 @@ using Model;
 
 namespace Controller
 {
-   public class RoomController
-   {
-      public Boolean CreateRoom(Room room)
-      {
-         // TODO: implement
-         return roomService.CreateRoom(room);
-      }
-      
-      public Boolean DeleteRoom(int id)
-      {
-         // TODO: implement
-         return roomService.DeleteRoom(id);
-      }
-      
-      public Boolean ReadRoom()
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean UpdateRoom(Room room)
-      {
-         // TODO: implement
-         return roomService.UpdateRoom(room);
-      }
-   
-      public RoomService roomService = new RoomService();
-   
-   }
+	public class RoomController
+	{
+		public Boolean CreateRoom(String RoomType, String RoomName)
+		{
+			return roomService.CreateRoom(RoomType, RoomName);
+		}
+		
+		public Boolean DeleteRoom(int id)
+		{
+			return roomService.DeleteRoom(id);
+		}
+		
+		public Boolean ReadRoom(int id)
+		{
+			return roomService.ReadRoom(id);
+		}
+		
+		public Boolean UpdateRoom(String RoomType, String RoomName, int id)
+		{
+			return roomService.UpdateRoom(RoomType, RoomName, id);
+		}
+	
+		public RoomService roomService = new RoomService();
+	
+	}
 }
