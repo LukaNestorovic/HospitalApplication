a6132a9df71b9b5b53376f294de7d51eaaa00b37
LukaNestorovic <lukanestorovic41@gmail.com>
2022-04-12 15:45:07 +0200

    .

--- Code/Novi/Controller/AppointmentController.cs
+++ Code/Novi/Controller/AppointmentController.cs
@@ -12,28 +12,43 @@ namespace Controller
 {
 	public class AppointmentController
 	{
-		public Boolean CreateApp(Appointment appointment)
+		public PatientService patientService = new PatientService();
+		public RoomService roomService = new RoomService();
+		public DoctorService doctorService = new DoctorService();
+		public Boolean CreateApp(DateTime dateTime, String description, int duration, Boolean emergency, int patientId, int doctorId, int roomId)
 		{
-			// TODO: implement
-			return appointmentService.CreateApp(appointment);
+			Patient patient = patientService.ReadPatient(patientId);
+			Doctor doctor = doctorService.ReadDoctor(doctorId);
+			Room room = roomService.ReadRoom(roomId);
+			return appointmentService.CreateApp(dateTime, description, duration, emergency, patient, doctor, room);
 		}
 		
 		public Boolean DeleteApp(int id)
 		{
-			// TODO: implement
 			return appointmentService.DeleteApp(id);
 		}
 		
-		public Boolean UpdateApp(Appointment appointment)
+		public Boolean UpdateApp(DateTime dateTime, String description, int duration, Boolean emergency, int patientId, int doctorId, int roomId, int appId)
 		{
-			// TODO: implement
-			return appointmentService.UpdateApp(appointment);
+			Patient patient = patientService.ReadPatient(patientId);
+			Doctor doctor = doctorService.ReadDoctor(doctorId);
+			Room room = roomService.ReadRoom(roomId);
+			return appointmentService.UpdateApp(dateTime, description, duration, emergency, patient, doctor, room, appId);
 		}
 		
-		public Boolean ReadApp()
+		public String[] ReadApp(int id)
 		{
-			// TODO: implement
-			return false;
+			Appointment appointment = appointmentService.ReadApp(id);
+			String[] appointmentDTO = new String[8];
+			appointmentDTO[0] = appointment.DateTime.ToString();
+			appointmentDTO[1] = appointment.Descripton;
+			appointmentDTO[2] = appointment.Duration.ToString();
+			appointmentDTO[3] = appointment.Emergency.ToString();
+			appointmentDTO[4] = appointment.patient.Id.ToString();
+			appointmentDTO[5] = appointment.doctor.Id.ToString();
+			appointmentDTO[6] = appointment.room.Id.ToString();
+			appointmentDTO[7] = appointment.Id.ToString();
+			return appointmentDTO;
 		}
 	
 		public AppointmentService appointmentService = new AppointmentService();
--- Code/Novi/Service/AppointmentService.cs
+++ Code/Novi/Service/AppointmentService.cs
@@ -6,36 +6,53 @@
 
 using System;
 using Model;
+using System.IO;
 
 namespace Service
 {
 	public class AppointmentService
 	{
-		public Boolean CreateApp(Appointment appointment)
+		public Boolean CreateApp(DateTime dateTime, String descripton, int duration, Boolean emergency, Patient patient, Doctor doctor, Room room)
 		{
-			// TODO: implement
-			return false;
+			int newID;
+			if (File.Exists(idFile))
+			{
+				newID = int.Parse(File.ReadAllText(idFile));
+				newID++;
+			}
+			else
+				newID = 0;
+			Appointment newAppointment = new Appointment(dateTime, descripton, duration, emergency, newID, patient, doctor, room);
+
+			File.WriteAllText(idFile, newID.ToString());
+
+			return appointmentRepository.Save(newAppointment);
 		}
 		
-		public Boolean UpdateApp(Appointment appointment)
+		public Boolean UpdateApp(DateTime dateTime, String descripton, int duration, Boolean emergency, Patient patient, Doctor doctor, Room room, int id)
 		{
-			// TODO: implement
-			return false;
+			Appointment appointment = appointmentRepository.FindByID(id);
+			appointment.DateTime = dateTime;
+			appointment.Descripton = descripton;
+			appointment.Duration = duration;
+			appointment.Emergency = emergency;
+			appointment.patient = patient;
+			appointment.doctor = doctor;
+			appointment.room = room;
+			return appointmentRepository.UpdateByID(appointment);
 		}
 		
 		public Boolean DeleteApp(int id)
 		{
-			// TODO: implement
-			return false;
+			return appointmentRepository.DeleteByID(id);
 		}
 		
 		public Appointment ReadApp(int id)
 		{
-			// TODO: implement
-			return null;
+			return appointmentRepository.FindByID(id);
 		}
 	
 		public Repository.AppointmentRepository appointmentRepository;
-	
+		public static String idFile = "appointmentID.txt";
 	}
 }
