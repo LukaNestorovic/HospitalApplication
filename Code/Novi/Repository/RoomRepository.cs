69709dafe1b78d0f2e49fb2c234e331f55050252
Jovan <savkovic.jovan@protonmail.com>
2022-04-12 13:28:58 +0200

    Zavrseni repozitorijumi

--- Code/Novi/Repository/RoomRepository.cs
+++ Code/Novi/Repository/RoomRepository.cs
@@ -1,6 +1,6 @@
 /***********************************************************************
- * Module:  RoomRepository.cs
- * Author:  lukaa
+ * Module:	RoomRepository.cs
+ * Author:	lukaa
  * Purpose: Definition of the Class Repository.RoomRepository
  ***********************************************************************/
 
@@ -10,39 +10,63 @@ using Model;
 
 namespace Repository
 {
-   public class RoomRepository
-   {
-      public List<Room> FindAll()
-      {
-         // TODO: implement
-         return null;
-      }
-      
-      public Room FindByID(int id)
-      {
-         // TODO: implement
-         return null;
-      }
-      
-      public Boolean Save(Room room)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean DeleteByID(int id)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean UpdateByID(Room room)
-      {
-         // TODO: implement
-         return false;
-      }
-   
-      private String FileName;
-   
-   }
+	public class RoomRepository
+	{
+	
+		public List<Room> FindAll()
+		{
+			return serializer.fromJSON(FileName);
+		}
+		
+		public Room FindByID(int id)
+		{
+			List<Room> all = serializer.fromJSON(FileName);
+			Room a = null;
+			foreach(Room i in all){
+				if(i.Id == id){
+					a = i;
+					break;
+				}
+			}
+			return a;
+		}
+		
+		public Boolean Save(Room appointment)
+		{
+			List<Room> all = serializer.fromJSON(FileName);
+			all.Add(appointment);
+			serializer.toJSON(FileName, all);
+			return true;
+		}
+		
+		public Boolean DeleteByID(int id)
+		{
+			List<Room> all = serializer.fromJSON(FileName);
+			foreach(Room i in all){
+				if(i.Id == id){
+					all.Remove(i);
+					break;
+				}
+			}
+			serializer.toJSON(FileName, all);
+			return true;
+		}
+		
+		public Boolean UpdateByID(Room appointment)
+		{
+			List<Room> all = serializer.fromJSON(FileName);
+			for(int i = 0; i < all.Count; i++){
+				if(all[i].Id == appointment.Id){
+					all[i] = appointment;
+					break;
+				}
+			}
+			serializer.toJSON(FileName, all);
+			return false;
+		}
+	
+		private static String FileName = "Rooms.json";
+		
+		private static Serializer<Room> serializer = new Serializer<Room>();
+	}
 }
