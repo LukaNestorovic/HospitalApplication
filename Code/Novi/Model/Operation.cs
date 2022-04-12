b42147d4e0eedc63052d1a85327ce4e10939f8d0
LukaAndric <81173650+LukaAndric@users.noreply.github.com>
2022-04-12 15:19:38 +0100

    update Operation

--- Code/Novi/Model/Operation.cs
+++ Code/Novi/Model/Operation.cs
@@ -15,56 +15,11 @@ namespace Model
 		public int Duration { get; set; }
 		public String Type { get; set; }
 		public int Id { get; set; }
-		
+		public Doctor doctor { get; set; }
 		public Patient patient { get; set; }
-		public System.Collections.ArrayList doctor;
-		
-		/// <pdGenerated>default getter</pdGenerated>
-		public System.Collections.ArrayList GetDoctor()
-		{
-			if (doctor == null)
-				doctor = new System.Collections.ArrayList();
-			return doctor;
-		}
-		
-		/// <pdGenerated>default setter</pdGenerated>
-		public void SetDoctor(System.Collections.ArrayList newDoctor)
-		{
-			RemoveAllDoctor();
-			foreach (Doctor oDoctor in newDoctor)
-				AddDoctor(oDoctor);
-		}
-		
-		/// <pdGenerated>default Add</pdGenerated>
-		public void AddDoctor(Doctor newDoctor)
-		{
-			if (newDoctor == null)
-				return;
-			if (this.doctor == null)
-				this.doctor = new System.Collections.ArrayList();
-			if (!this.doctor.Contains(newDoctor))
-				this.doctor.Add(newDoctor);
-		}
-		
-		/// <pdGenerated>default Remove</pdGenerated>
-		public void RemoveDoctor(Doctor oldDoctor)
-		{
-			if (oldDoctor == null)
-				return;
-			if (this.doctor != null)
-				if (this.doctor.Contains(oldDoctor))
-					this.doctor.Remove(oldDoctor);
-		}
-		
-		/// <pdGenerated>default removeAll</pdGenerated>
-		public void RemoveAllDoctor()
-		{
-			if (doctor != null)
-				doctor.Clear();
-		}
 		public Room room { get; set; }
 
-		  public Operation(DateTime dateTime, int duration, String type, int id, Patient patient, ArrayList doctor, Room room)
+		  public Operation(DateTime dateTime, int duration, String type, int id, Patient patient, Doctor doctor, Room room)
 		  {
 				DateTime = dateTime;
 				Duration = duration;
--- Code/Novi/Service/OperationService.cs
+++ Code/Novi/Service/OperationService.cs
@@ -1,41 +1,58 @@
 /***********************************************************************
- * Module:  OperationService.cs
- * Author:  lukaa
- * Purpose: Definition of the Class Service.OperationService
+ * Module:	RoomService.cs
+ * Author:	lukaa
+ * Purpose: Definition of the Class Service.RoomService
  ***********************************************************************/
 
 using System;
 using Model;
+using Repository;
+using System.IO;
 
-namespace Service
+namespace Operation
 {
-   public class OperationService
-   {
-      public Boolean CreateOperation(Operation operation)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean UpdateOperation(Operation operation)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Boolean DeleteOperation(int id)
-      {
-         // TODO: implement
-         return false;
-      }
-      
-      public Operation ReadOperation(int id)
-      {
-         // TODO: implement
-         return null;
-      }
-   
-      public Repository.OperationRepository operationRepository;
-   
-   }
+	public class OperationService
+	{
+		public Boolean CreateOperation(DateTime dateTime, int duration, String type, Patient patient, Doctor doctor, Room room)
+		{
+			int newID;
+			if (File.Exists(idFile))
+			{
+				newID = int.Parse(File.ReadAllText(idFile));
+				newID++;
+			}
+			else
+				newID = 0;
+			Operation newOperation = new Operation(dateTime, duration, type, patient, doctor, room, newID);
+
+			File.WriteAllText(idFile, newID.ToString());
+
+			return operationRepository.Save(newOperation);
+		}
+
+		public Boolean UpdateOperation(DateTime dateTime, int duration, String type, Patient patient, Doctor doctor, Room room, int id)
+		{
+			Operation operation = operationRepository.FindByID(id);
+			operation.OperationDateTime = dateTime;
+			operation.Duration = duration;
+			operation.Type = type;
+			operation.Patient = patient;
+			operation.Doctor = doctor;
+			operation.Room = room;
+			return operationRepository.UpdateByID(operation);
+		}
+
+		public Boolean DeleteOperation(int id)
+		{
+			return operationRepository.DeleteByID(id);
+		}
+
+		public Room ReadOperation(int id)
+		{
+			return operationRepository.FindByID(id);
+		}
+
+		public static Repository.OperationRepository operationRepository = new OperationRepository();
+		public static String idFile = "operationID.txt";
+	}
 }
