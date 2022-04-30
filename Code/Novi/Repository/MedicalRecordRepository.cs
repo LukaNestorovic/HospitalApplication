ab8c35b79b665df094565e41267e52317b25e97b
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2022-04-30 21:39:45 +0200

    Kreiran Medical Record Repository

---
+++ Code/Novi/Repository/MedicalRecordRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serialization;
+using Model;
+
+namespace Repository
+{
+    public class MedicalRecordRepository
+    {
+		public Boolean UpdateByPatient(MedicalRecord medicalRecord)
+		{
+			List<MedicalRecord> all = serializer.fromJSON(FileName);
+			for (int i = 0; i < all.Count; i++)
+			{
+				if (all[i].patient == medicalRecord.patient)
+				{
+					all[i] = medicalRecord;
+					break;
+				}
+			}
+			serializer.toJSON(FileName, all);
+			return false;
+		}
+
+		private static String FileName = @"..\..\..\data\MedicalRecords.json";
+
+		private static Serializer<MedicalRecord> serializer = new Serializer<MedicalRecord>();
+	}
+}
