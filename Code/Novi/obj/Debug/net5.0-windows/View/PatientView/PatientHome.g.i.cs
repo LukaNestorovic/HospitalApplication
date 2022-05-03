95e353e5b33dea15fe18f44e2eefaa509335a5f8
LukaNestorovic <lukanestorovic41@gmail.com>
2022-05-03 15:04:40 +0200

    .

--- Code/Novi/obj/Debug/net5.0-windows/View/PatientView/PatientHome.g.i.cs
+++ Code/Novi/obj/Debug/net5.0-windows/View/PatientView/PatientHome.g.i.cs
@@ -47,13 +47,13 @@ namespace ProjekatSIMS.View.PatientView {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProjekatSIMS;component/view/patientview/patienthome.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProjekatSIMS;V1.0.0.0;component/view/patientview/patienthome.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\PatientView\PatientHome.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace ProjekatSIMS.View.PatientView {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
