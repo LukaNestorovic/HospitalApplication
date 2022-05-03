2e9bb6d62ab8d322c866bc5a67cee397ae4bad29
LukaNestorovic <lukanestorovic41@gmail.com>
2022-05-03 19:41:34 +0200

    .

--- Code/Novi/obj/Debug/net5.0-windows/View/DoctorView/Delete.g.i.cs
+++ Code/Novi/obj/Debug/net5.0-windows/View/DoctorView/Delete.g.i.cs
@@ -61,7 +61,7 @@ namespace ProjekatSIMS.View.DoctorView {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProjekatSIMS;component/view/doctorview/delete.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProjekatSIMS;V1.0.0.0;component/view/doctorview/delete.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\DoctorView\Delete.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
