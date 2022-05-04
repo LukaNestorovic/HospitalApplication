aedb39c42ccc0f9f93f7c01554c140354269756e
LukaNestorovic <lukanestorovic41@gmail.com>
2022-05-04 11:15:01 +0200

    .

---
+++ Code/Novi/Controller/DeanController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Service;
+
+namespace Controller
+{
+    public class DeanController
+    {
+        public Dean ReadDeanByEmail(String email)
+        {
+            return deanService.ReadDeanByEmail(email);
+        }
+        public Dean ReadDeanByPassword(String password)
+        {
+            return deanService.ReadDeanByPassword(password);
+        }
+        public DeanService deanService = new DeanService();
+    }
+}
