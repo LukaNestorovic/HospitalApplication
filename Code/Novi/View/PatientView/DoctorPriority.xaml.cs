a976fdf43aa14a65ee0f6f7f25193109623543e3
LukaNestorovic <lukanestorovic41@gmail.com>
2022-05-14 17:46:33 +0200

    hci

--- Code/Novi/View/PatientView/DoctorPriority.xaml.cs
+++ Code/Novi/View/PatientView/DoctorPriority.xaml.cs
@@ -25,17 +25,22 @@ namespace ProjekatSIMS.View.PatientView
         private int id;
         public Appointment appointment = new Appointment();
         public AppointmentController appointmentController = new AppointmentController();
+        public DoctorController doctorController = new DoctorController();
+        public List<Model.Doctor> doctors = new List<Model.Doctor>();
+        public Model.Doctor doctor = new Model.Doctor();
         public DoctorPriority(int id)
         {
             InitializeComponent();
+            doctors = doctorController.ReadAll();
+            Combo.ItemsSource = doctors;
             this.id = id;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             DateTime date = DatePicker.SelectedDate.GetValueOrDefault();
-            int doctorId = Int32.Parse(Id.Text);
-            appointment = appointmentController.ReadWithPriorityDoctor(doctorId, date);
+            doctor = (Model.Doctor)Combo.SelectedItem;
+            appointment = appointmentController.ReadWithPriorityDoctor(doctor.Id, date);
             appointmentController.UpdateApp(appointment.DateTime, appointment.Descripton, appointment.Duration, appointment.Emergency, id, appointment.Doctor.Id, 1, appointment.Id);
             var s = new PatientView(id);
             s.Show();
