520b9a4deb21385f370585417de74f2b4cd7b521
LukaNestorovic <lukanestorovic41@gmail.com>
2022-05-14 23:26:17 +0200

    .

--- Code/Novi/Repository/DeanRepository.cs
+++ Code/Novi/Repository/DeanRepository.cs
@@ -10,38 +10,5 @@ namespace Repository
 {
     public class DeanRepository
     {
-/*		public Dean FindByEmail(String email)
-		{
-			List<Dean> all = serializer.fromJSON(FileName);
-			Dean a = null;
-			foreach (Dean i in all)
-			{
-				if (i.Email == email)
-				{
-					a = i;
-					break;
-				}
-			}
-			return a;
-		}
-
-		public Dean FindByPassword(String password)
-		{
-			List<Dean> all = serializer.fromJSON(FileName);
-			Dean a = null;
-			foreach (Dean i in all)
-			{
-				if (i.Password == password)
-				{
-					a = i;
-					break;
-				}
-			}
-			return a;
-		}*/
-
-		private static String FileName = @"..\..\..\data\Dean.json";
-
-		private static Serializer<Dean> serializer = new Serializer<Dean>();
 	}
 }
--- Code/Novi/Repository/PrescriptionRepository.cs
+++ Code/Novi/Repository/PrescriptionRepository.cs
@@ -16,36 +16,7 @@ namespace Repository
             serializer.toJSON(FileName, all);
             return true;
         }
-
- /*       public List<Prescription> FindAllByPatientId(int id)
-        {
-            List<Prescription> all = serializer.fromJSON(FileName);
-            List<Prescription> ret = new List<Prescription>();
-            foreach (Prescription i in all)
-            {
-                if (i.patient == null)
-                {
-                    continue;
-                }
-                if (i.patient.Id == id)
-                {
-                    ret.Add(i);
-                }
-            }
-            foreach(Prescription i in ret)
-            {
-                DateTime now = DateTime.Now;
-                TimeSpan value = i.datetime.Subtract(now);
-                if(value.TotalMinutes < 15 && value.TotalMinutes > 0)
-                {
-                    MessageBox.Show("Za manje od 15 minuta treba da popijete lek " + i.drug.Name, "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-            }
-            return ret;
-        }*/
-
         
-
         private static String FileName = @"..\..\..\data\Prescriptions.json";
 
         private static Serializer<Prescription> serializer = new Serializer<Prescription>();
