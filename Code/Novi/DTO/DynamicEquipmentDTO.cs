9337398b557625edb8664cccf40908c50db9c72c
SalipurNikola <salipur.in16.2019@uns.ac.rs>
2022-05-16 17:20:18 +0200

    DynamicEquipment

---
+++ Code/Novi/DTO/DynamicEquipmentDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DynamicEquipmentDTO
+    {
+        public String Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateOfOrder { get; set; }
+
+        public DynamicEquipmentDTO(string name, int quantity, DateTime dateOfOrder)
+        {
+            Name = name;
+            Quantity = quantity;
+            DateOfOrder = dateOfOrder;
+        }
+
+        public DynamicEquipmentDTO()
+        {
+        }
+    }
+}
---
+++ Code/Novi/Service/DynamicEquipmentService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Repository;
+using DTO;
+using System.IO;
+
+namespace Service
+{
+    public class DynamicEquipmentService
+    {
+
+		public int createId()
+        {
+			int newID;
+			if (File.Exists(idFile))
+			{
+				newID = int.Parse(File.ReadAllText(idFile));
+				newID++;
+			}
+			else
+				newID = 0;
+				File.Create(idFile).Close();
+			File.WriteAllText(idFile, newID.ToString());
+			id = newID;
+			return newID;
+		}
+		public Boolean CreateDynamicalEquipment(DynamicEquipmentDTO dynamicEquipmentDTO)
+		{
+			int newID = createId();
+			String name  = dynamicEquipmentDTO.Name;
+			int quantity = dynamicEquipmentDTO.Quantity;
+			DateTime dateOfOrder = dynamicEquipmentDTO.DateOfOrder;
+
+			DynamicEquipment dynamicEquipment = new DynamicEquipment(name, quantity,dateOfOrder, newID);
+			return dynamicEquipmentRepository.Save(dynamicEquipment);
+		}
+
+		public List<DynamicEquipment> ReadAllInStorage()
+        {
+			return dynamicEquipmentRepository.ReadAllInStorage();
+        }
+
+		public int id = 0;
+		public DynamicEquipmentRepository dynamicEquipmentRepository = new DynamicEquipmentRepository();
+		public String idFile = @"..\..\..\Data\dynamicEquipmentID.txt";
+	}
+}
